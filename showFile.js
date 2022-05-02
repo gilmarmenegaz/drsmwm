a4fbad159c9ea095d0e55eb5f6fcf916bd7ce990
gilmarmenegaz <gilmar.menegaz@gmail.com>
2022-05-02 19:46:46 -0300

    comit 2 testando

---
+++ showFile.js
@@ -0,0 +1,37 @@
+function showFile(pdf, embedMode) {
+    /* Initialize the AdobeDC View object */
+    var adobeDCView = new AdobeDC.View({
+        /* Pass your registered client id */
+        clientId: "<YOUR_CLIENT_ID>"
+    });
+
+    /* Pass the embed mode option here */
+    const viewerConfig = {
+        embedMode: embedMode
+    };
+
+    /* Invoke the file preview API on Adobe DC View object */
+    adobeDCView.previewFile({
+        /* Pass information on how to access the file */
+        content: {
+            /* Location of file where it is hosted */
+            location: {
+                url: "https://documentcloud.adobe.com/view-sdk-demo/PDFs/" + pdf,
+                /*
+                If the file URL requires some additional headers, then it can be passed as follows:-
+                header: [
+                    {
+                        key: "<HEADER_KEY>",
+                        value: "<HEADER_VALUE>",
+                    }
+                ]
+                */
+            },
+        },
+        /* Pass meta data of file */
+        metaData: {
+            /* file name */
+            fileName: pdf
+        }
+    }, viewerConfig);
+}
